49bcdfae504675f5684eba99f865f7fe9b313885
MadeReihan <reihanmade@gmail.com>
2022-01-17 21:13:40 +0700

    beneran final

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,4 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/facilityUser',[facilityUser::class, 'index'])->middleware('auth');
 
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
